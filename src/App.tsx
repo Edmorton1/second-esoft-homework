ad92110470ecfdc69c8eddf71c8293fd4a40d105
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-04-23 11:24:00 +0500

    finaly

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,65 @@
-import { useEffect, useState } from "react"
+import { memo, useEffect, useState } from "react"
+
+const Header = memo(() => {
+  console.log("Header render")
+  return <h1>Это мой первый React. проект!</h1>
+})
+
+const Clock = memo(() => {
+  const [time, setTime] = useState<Date>(new Date())
+  
+  useEffect(() => {
+    const timeInterval = setInterval(() => setTime(new Date()), 1000)
+
+    return () => clearInterval(timeInterval)
+  }, [])
+  
+  return (
+    <>
+      <div>{time.toLocaleDateString()} {time.toLocaleTimeString()}</div>
+      {time.getMinutes() % 5 == 0 ? <div>Время делится на 5</div> : null}
+    </>
+  )
+})
+
+const Greeting = ({name}: {name: string}) => {
+  const [render, setRender] = useState(0)
+  console.log('Greeting render')
+
+  useEffect(() => {
+    setRender(render => render + 1)
+    if (render === 0) {
+      console.log(`Первый рендер ${name}`, render)
+    } else {
+      console.log(`Поменялся пропс ${name}`, render)
+    }
+  }, [name])
+
+  return <div>{render <= 2 ? `Привет, ${name}!` : `Привет, у тебя поменялось имя, теперь ты ${name}!`}</div>
+}
+
+const Container = () => {
+  const names = ['Vasya', 'Petya', 'Ivan', 'Lexa', 'Kirill']
+  const getRandomName = () => names[Math.floor(Math.random() * 5)]
+  const [name, setName] = useState<string>(getRandomName)
+
+  useEffect(() => {
+    const nameInterval = setInterval(() => setName(getRandomName()), 10000)
+    
+    return () => clearInterval(nameInterval)
+  }, [])
+
+
+  return (
+    <>
+      <Header />
+      <Greeting name={name} />
+      <Clock />
+    </>
+  )
+}
 
 function App() {
-
-  function Header() {
-    const [name, setName] = useState(null)
-    return <div></div>
-  }
-
-  function Clock() {
-    const [name, setName] = useState(null)
-    return <div></div>
-  }
-
-  function Greeting() {
-    const [name, setName] = useState(null)
-    return <div></div>
-  }
-
-  function Container() {
-    const names = ['Vasya', 'Petya', 'Ivan', 'Lexa', 'Kirill']
-    const getRandomName = () => names[Math.floor(Math.random() * 5)]
-    const [name, setName] = useState<string | null>(null)
-
-    useEffect(() => {
-      setInterval(() => setName(getRandomName()), 100)
-      clearInterval()
-    }, [])
-
-
-    return (
-    <div>{name}</div>
-  )
-  }
 
   return (
     <Container />
